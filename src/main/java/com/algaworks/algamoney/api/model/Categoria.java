f1e8a1d4e2fd198161da6e7bdf3e8238f515d04e
Osmael <osmaelsousa.ti@gmail.com>
2019-09-03 21:35:44 -0300

    Consultando recurso categoria com get

---
+++ src/main/java/com/algaworks/algamoney/api/model/Categoria.java
@@ -0,0 +1,67 @@
+package com.algaworks.algamoney.api.model;
+	
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+	
+@Entity
+@Table(name = "categoria")
+@SequenceGenerator(
+		name=Categoria.CATEGORIA_SEQUENCE,
+		sequenceName = Categoria.CATEGORIA_SEQUENCE, allocationSize = 1
+		)
+public class Categoria {
+	
+	public static final String CATEGORIA_SEQUENCE = "categoria_sequence";
+	
+	@Id
+	@GeneratedValue(strategy =  GenerationType.SEQUENCE, generator = CATEGORIA_SEQUENCE)
+	private Long codigo;
+	
+	private String nome;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
+}
