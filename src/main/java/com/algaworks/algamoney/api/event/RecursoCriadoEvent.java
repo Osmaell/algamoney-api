e47f9ec5019ce0a4fe4631ef1237e0cdb54b6d90
Osmael <osmaelsousa.ti@gmail.com>
2019-10-27 23:50:03 -0300

    Criando um evento para adicionar o header location

---
+++ src/main/java/com/algaworks/algamoney/api/event/RecursoCriadoEvent.java
@@ -0,0 +1,35 @@
+package com.algaworks.algamoney.api.event;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationEvent;
+
+/**
+ * Classe que representa um {@link ApplicationEvent} 
+ * para quando um recurso for criado.
+ * 
+ * @author Osmael
+ *
+ */
+public class RecursoCriadoEvent extends ApplicationEvent {
+
+	private static final long serialVersionUID = 1L;
+
+	private HttpServletResponse response;
+	private Long codigo;
+
+	public RecursoCriadoEvent(Object source, HttpServletResponse response, Long codigo) {
+		super(source);
+		this.response = response;
+		this.codigo = codigo;
+	}
+
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+}
