3c88a539cc510475fb91760862a018c0b027786b
osmaelsousa.ti@gmail.com <ericathauannejva@gmail.com>
2020-05-15 21:53:48 -0300

    gerando os bytes do relatorio por pessoa

--- src/main/java/com/algaworks/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/algaworks/algamoney/api/service/LancamentoService.java
@@ -1,13 +1,28 @@
 package com.algaworks.algamoney.api.service;
 	
+import java.io.InputStream;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.algaworks.algamoney.api.dto.LancamentoEstatisticaPessoa;
 import com.algaworks.algamoney.api.model.Lancamento;
 import com.algaworks.algamoney.api.model.Pessoa;
 import com.algaworks.algamoney.api.repository.LancamentoRepository;
 import com.algaworks.algamoney.api.repository.PessoaRepository;
 import com.algaworks.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 	
 @Service
 public class LancamentoService {
@@ -26,6 +41,22 @@ public class LancamentoService {
 		}
 		
 		return lancamentoRepository.save(lancamento);
+	}
+	
+	public byte[] relatorioPorPessoa(LocalDate inicio, LocalDate fim) throws JRException {
+		
+		List<LancamentoEstatisticaPessoa> dados = lancamentoRepository.porPessoa(inicio, fim);
+		
+		Map<String, Object> parametros = new HashMap<>();
+		parametros.put("DT_INICIO", Date.valueOf(inicio));
+		parametros.put("DT_FIM", Date.valueOf(fim));
+		parametros.put("REPORT_LOCALE", new Locale("pt", "BR"));
+		
+		InputStream inputStream = this.getClass().getResourceAsStream("/relatorios/lancamentos-por-pessoa.jasper");
+		
+		JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, parametros, new JRBeanCollectionDataSource(dados));
+		
+		return JasperExportManager.exportReportToPdf(jasperPrint);
 	}
 	
 }
